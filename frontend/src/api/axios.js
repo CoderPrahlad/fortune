64f2e878c4c83a4afacbf2f5f973f71d5be79fc7
Prahlad Sharma <79293553+CoderPrahlad@users.noreply.github.com>
2026-03-31 08:25:50 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://fortune-go65.onrender.com/api',
   headers: { 'Content-Type': 'application/json' }
 });
 
